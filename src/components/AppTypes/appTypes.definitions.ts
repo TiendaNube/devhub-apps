3904963bea65c1ced4674aa0dbe4a13b1cc3e01b
Diego Silvério <73891463+diegopsilverio@users.noreply.github.com>
2023-09-04 15:08:07 -0300

    Update appTypes.definitions.ts

--- src/components/AppTypes/appTypes.definitions.ts
+++ src/components/AppTypes/appTypes.definitions.ts
@@ -4,16 +4,16 @@ import { AppsIcon, AppsListIcon, IconProps } from "@nimbus-ds/icons";
 export const appTypes = [
   {
     icon: AppsIcon as FC<IconProps>,
-    title: "Nativo",
+    title: "Aplicativos incorporados ao administrador",
     description:
-      "Os aplicativos nativos, por outro lado, oferecem uma integração mais profunda com a plataforma Nuvemshop. Eles são desenvolvidos para se integrar diretamente ao painel de administração dos merchants, proporcionando uma experiência contínua e fluida. Ao utilizar ferramentas como o nosso design system Nimbus, garantimos que a experiência visual e de usabilidade permaneça consistente, mesmo quando o merchant está interagindo com um aplicativo de parceiro externo. Isso permite que o merchant desfrute de recursos adicionais sem deixar o ambiente familiar do painel de administração da Nuvemshop.",
+      "Os aplicativos incorporados ao adminstrador, por outro lado, oferecem uma integração mais profunda com a plataforma Nuvemshop. Eles são desenvolvidos para se integrar diretamente ao painel de administração dos lojistas, proporcionando uma experiência contínua e fluida. Ao utilizar ferramentas como o nosso design system Nimbus, garantimos que a experiência visual e de usabilidade permaneça consistente, mesmo quando o lojista está interagindo com um aplicativo de parceiro externo. Isso permite que o lojista desfrute de recursos adicionais sem deixar o ambiente familiar do painel de administração da Nuvemshop.",
     link: "/docs/applications/native",
   },
   {
     icon: AppsListIcon as FC<IconProps>,
-    title: "Standalone",
+    title: "Aplicativos Externos",
     description:
-      "Os aplicativos standalone são soluções independentes, desenvolvidas externamente à Nuvemshop. Esses aplicativos operam de forma autônoma e podem abranger uma ampla gama de funcionalidades, desde marketing até gerenciamento de estoque. Eles proporcionam uma experiência única e podem ser uma ótima maneira de adicionar recursos especializados às lojas dos merchants.",
+      "Os aplicativos externos são soluções independentes, desenvolvidas externamente à Nuvemshop. Esses aplicativos operam de forma autônoma e podem abranger uma ampla gama de funcionalidades, desde marketing até gerenciamento de estoque. Eles proporcionam uma experiência única e podem ser uma ótima maneira de adicionar recursos especializados às lojas dos lojista.",
     link: "/docs/applications/standalone",
   },
 ];
